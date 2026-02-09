077f91f72cdda11a1cb73a7bfa8f5301b8a62e72
tcmy6526512 <thomaschampy00@gmail.com>
2026-02-10 00:44:39 +0100

    fix: .sh

--- scripts/docs-build.sh
+++ scripts/docs-build.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")/.."
 python3 -m venv .venv-docs
 source .venv-docs/bin/activate
 
+python -m pip install --upgrade pip setuptools wheel
 pip install -r documentations/requirements.txt
 mkdocs build -f documentations/mkdocs.yml
 
--- scripts/docs-serve.sh
+++ scripts/docs-serve.sh
@@ -6,5 +6,6 @@ cd "$(dirname "$0")/.."
 python3 -m venv .venv-docs
 source .venv-docs/bin/activate
 
+python -m pip install --upgrade pip setuptools wheel
 pip install -r documentations/requirements.txt
 mkdocs serve -f documentations/mkdocs.yml -a 127.0.0.1:8000
