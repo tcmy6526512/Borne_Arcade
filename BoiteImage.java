e369f0471ca3afefc330ec1a9655a0fbcfb1fe4e
tcmy6526512 <thomaschampy00@gmail.com>
2026-02-12 11:17:56 +0100

    fix: jeux sans photo_small.png

--- BoiteImage.java
+++ BoiteImage.java
@@ -5,11 +5,16 @@ import MG2D.geometrie.Texture;
 
 public class BoiteImage extends Boite{
 
+    private static final String IMAGE_PAR_DEFAUT = "img/fond3.png";
     Texture image;
 
     BoiteImage(Rectangle rectangle, String image) {
 	super(rectangle);
-	this.image = new Texture(image+"/photo_small.png", new Point(760, 648));
+    String cheminImage = image + "/photo_small.png";
+    if (!(new java.io.File(cheminImage).exists())) {
+        cheminImage = IMAGE_PAR_DEFAUT;
+    }
+    this.image = new Texture(cheminImage, new Point(760, 648));
     }
 
     public Texture getImage() {
@@ -17,7 +22,12 @@ public class BoiteImage extends Boite{
     }
 
     public void setImage(String chemin) {
-	this.image.setImg(chemin+"/photo_small.png");
+    String cheminImage = chemin + "/photo_small.png";
+    if (!(new java.io.File(cheminImage).exists())) {
+        this.image.setImg(IMAGE_PAR_DEFAUT);
+    } else {
+        this.image.setImg(cheminImage);
+    }
 	//this.image.setTaille(400, 320);
     }
 
