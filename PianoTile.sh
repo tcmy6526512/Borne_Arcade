90ea5b0cf27593e686c8d69ce143e7a13cf75f99
tcmy6526512 <thomaschampy00@gmail.com>
2026-03-03 16:44:36 +0100

    fix: suppr librosa

--- PianoTile.sh
+++ PianoTile.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 cd "$(dirname "$0")"
 cd projet/PianoTile
 
-if ! python3 -c "import pygame, librosa" >/dev/null 2>&1; then
- 	 echo "[PianoTile] Installation de pygame..."
- 	 python3 -m pip install --user pygame
+if ! python3 -c "import pygame" >/dev/null 2>&1; then
+	echo "[PianoTile] Installation de pygame..."
+	python3 -m pip install --user pygame
 fi
 
 python3 app/game.py
